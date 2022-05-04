c1ddd0507bc9444388eea23ebb5cb8befb0d7d6d
raunitarquiniomuniz <raunitarquiniomuniz@gmail.com>
2022-05-04 09:47:13 -0300

    aula10

---
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,19 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		//Convenção JAVA
+		int idade = 32;
+		String nome = "Rauni";
+		String nomeCachorro = "estopinha";
+		String anoAtual = "2022";
+		
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		System.out.println("Nome do Cachorro = " + nomeCachorro);
+		System.out.println("Ano Atual = " + anoAtual);
+	}
+
+}
